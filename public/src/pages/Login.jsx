a8c86ad1dd2faf6b75a9bb916260bfdf7aa806c8
Pule-Prince <pulemabote01@gmail.com>
2023-07-18 07:23:36 +0200

    Register frontend page

---
+++ public/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
+
+
+const Login = () => {
+    return (
+        <div>Login</div>
+    );
+};
+
+export default Login;
